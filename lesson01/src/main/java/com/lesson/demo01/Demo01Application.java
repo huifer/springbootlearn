e457df66fb18110b43ea5652001cf8a0087a51fe
wangtao <1187982580@qq.com>
2018-08-31 07:54:09 +0800

    Spring Boot

--- lesson01/src/main/java/com/lesson/demo01/Demo01Application.java
+++ lesson01/src/main/java/com/lesson/demo01/Demo01Application.java
@@ -2,8 +2,6 @@ package com.lesson.demo01;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
